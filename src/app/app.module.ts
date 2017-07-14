ca31fcb17d10cb9cdc135e8fd933cdd8b7dcbb2a
AMSanti <darkstars31@gmail.com>
2017-07-14 15:57:00 -0500

    Added HttpModule for the next section of the tut

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { TacoComponent } from './taco.component';
@@ -17,7 +18,8 @@ import { TacoService } from './taco.service';
     DashboardComponent
   ],
   imports: [
-    BrowserModule,
+    BrowserModule,,
+    HttpModule,
     RouterModule.forRoot([ 
       {        path: '',                  redirectTo: '/dashboard',        pathMatch: 'full'      },
       {        path: 'tacos',             component: TacoComponent       }, 
